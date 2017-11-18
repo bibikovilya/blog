96339d8e6bd2dcb77e8fe98477f5b63b64dcbd39
Ilya Bibikov <ib@anadeainc.com>
2017-11-18 17:13:31 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+POST_COUNT = 200
+USER_COUNT = 100
+IP_COUNT = 50
+RATE_COUNT = 150
+
+users_ids = (1..USER_COUNT).to_a
+ips = (1..IP_COUNT).to_a
+
+POST_COUNT.times do |i|
+  PostServices::Create.new(title: "Title#{i}",
+                           body: "Body##{i}",
+                           login: "user##{users_ids.sample}",
+                           ip: "128.0.0.#{ips.sample}").call
+end
+
+post_ids = Post.pluck(:id)
+
+RATE_COUNT.times do
+  PostServices::AddMark.new(post_id: post_ids.sample, mark: rand(1..5)).call
+end
